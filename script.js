2c1f70fb5e395271e71ece31f8e8a98d5ad92bd4
Meurer531 <148451701+Meurer531@users.noreply.github.com>
2025-02-28 12:04:10 -0300

    .

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', () => {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuiFonteBotao.addEventListener('click', () => {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
